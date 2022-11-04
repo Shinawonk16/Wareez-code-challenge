e552d0c396a7ceca31e942a148a8b370a59de937
Shinawonk16 <shinawonk@gmail.com>
2022-11-04 04:45:55 -0700

    first commit

---
+++ Question 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiplyOriginal();
+            
+        }
+        static void MultiplyOriginal()
+        {
+            int[]myArray = {5,3,6,1,12};
+            // int original = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i]* 2 == myArray[i+1])
+                {
+                     if (myArray[i+1]*2 == myArray[i+2])
+                    {
+                        if (myArray[i+3]*2 ==myArray[i+4] )
+                        {
+                            System.Console.WriteLine($"{myArray[i+4] * 2}");
+                        }
+                    }
+                    else if (myArray[i+2]*2 == myArray[i+3])
+                    {
+                            System.Console.WriteLine($"{myArray[i+3]*2}");
+                    }
+                   
+                    
+                }
+                else if (myArray[i+1]*2 == myArray[i+2])
+                {
+                    if (myArray[i+2]*2 == myArray[i+3])
+                    {
+                        if (myArray[i+3]*2 ==myArray[i+4] )
+                        {
+                            System.Console.WriteLine($"{myArray[i+4]* 2} ");
+                        }
+                    }
+                    else if (myArray[i+2]*2 == myArray[i+4])
+                    {
+                        System.Console.WriteLine($"{myArray[i+4]*2}");
+                    }
+                }
+                // else
+                // {
+                //     // System.Console.WriteLine($"{myArray[i]* 2}");
+                // }
+            }        
+            
+
+        }
+    }
+}        
---
+++ Question 2/Program.cs
@@ -0,0 +1,76 @@
+﻿int[]lonelyArray = {10,6,5,8};
+for (int i = 0; i <lonelyArray.Length; i++)
+{
+    if (lonelyArray[i]-1 ==lonelyArray[i+1] || lonelyArray[i]+1 ==lonelyArray[i+1] || lonelyArray[i] == lonelyArray[i+1])
+    {
+        System.Console.WriteLine($"{lonelyArray[i]} is not lonely");
+    }
+    else
+    {
+        System.Console.WriteLine($"{lonelyArray[i]} are lonely");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
